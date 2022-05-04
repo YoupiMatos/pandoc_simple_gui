4ef5146ffb6ff99a5e2e58c36771ebfdf35fe9a6
Mattéo Tatibouët <matteo.tatibouet@assurance-maladie.fr>
2022-05-04 11:17:33 +0200

    Application un peu moins dégueulasse
    Prise en charge des erreurs liées à manque de pdflatex sur la machine
    Layout en colonnes fonctionnel

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from window import window
 import os.path
 import subprocess
 
+def getInputFormats():
+    input_formats = subprocess.check_output(["powershell", "-Command", "pandoc --list-input-formats"])
+    input_formats = str(input_formats)
+    input_list = []
+    input_list = input_formats.split(r"\r\n")
+    input_list[0] = input_list[0].replace("'b'", "")
+    input_list.pop()
+
+    print(input_list)
+    print(len(input_list))
+    
+    return input_list
+
 def getOutputFormats():
-    output_formats = subprocess.check_output(["powershell", "-Command", "pandoc --list-input-formats"])
+    output_formats = subprocess.check_output(["powershell", "-Command", "pandoc --list-output-formats"])
     output_formats = str(output_formats)
     output_list = []
-    final_list = []
     output_list = output_formats.split(r"\r\n")
     output_list[0] = output_list[0].replace("'b'", "")
     output_list.pop()
@@ -35,11 +47,11 @@ def convert(entry_file: str, output_folder: str, name: str):
 def getResult(result: subprocess.CompletedProcess):
     if result.returncode == 0:
         pass
-
-
+input_formats = getInputFormats()
+output_formats = getOutputFormats()
 while True:     #Main event loop
-    output_formats = getOutputFormats()
-    window['format_list'].update(output_formats)
+    window['input_format_list'].update(values=input_formats)
+    window['output_format_list'].update(values=output_formats)
     event, values = window.read()
     
     if event in (None, 'Exit'):
@@ -51,5 +63,9 @@ while True:     #Main event loop
 
         result = convert(input_file, output_folder, name)
         if result.returncode == 0:
-            window.Element(key='last_result')
+            window.Element(key='last_result').update("Conversion réussie!")
+        elif "pdflatex" in str(result.stderr):
+            window.Element(key='last_result').update("Échec de la conversion. Il faut installer pdflatex pour convertir vers le format pdf.")
+        else:
+            window.Element(key='last_result').update("Échec de la conversion.")
         print(result)
--- window.py
+++ window.py
@@ -1,59 +1,53 @@
 import PySimpleGUI as psg
 
 file_list_column = [
-
-    [
         psg.Text("Document à convertir"),
         psg.In(size=(25, 1), enable_events=True, key="input_file"),
         psg.FileBrowse(),
 
-    ]
-
 ]
 
 outputFolder = [
-    [
         psg.Text("Dossier de sortie"),
         psg.In("dossier de sortie...", size=(25, 1), enable_events=True, key="output_folder", ),
         psg.FolderBrowse()
-    ]
 ]
 
 converted_filename = [
-    [
         psg.Text("Nom du fichier converti"),
         psg.Input('fichier de sortie', size=(25,1), enable_events=True, key="name")
-    ]
 ]
 
 confirm_button = [
-    [
         psg.Button('Convertir', key="convert")
-    ]
 ]
 
 last_result = [
-    [
         psg.Text('Dernier résultat : '),
         psg.Text('', key='last_result')
-    ]
 ]
 
-format_list = [
-    [
+output_format_list = [
         psg.Text("Formats de sortie :"),
-        psg.Listbox([], key='format_list', enable_events=True, size=(25,25))
-    ]
+        psg.Combo([], key='output_format_list', enable_events=True, size=(25,25))
 ]
 
-layout = [
-    [psg.Column(file_list_column)],
-    [psg.Column(outputFolder)],
-    [psg.Column(converted_filename)],
-    [psg.Column(format_list)],
-    [psg.Column(confirm_button)],
-    [psg.Column(last_result)]
+input_format_list = [
+        psg.Text("Formats d'entrée :"),
+        psg.Combo([], key='input_format_list', enable_events=True, size=(25,25))
 ]
 
+format_col = [input_format_list, output_format_list]
+
+layout = [[ 
+        psg.Column([
+            file_list_column,
+            outputFolder,
+            converted_filename,
+            confirm_button,
+            last_result,
+        ]),
+        psg.Column(format_col)
+]]
 
 window = psg.Window("Pandoc Simple GUI", layout, margins=(100, 50), resizable=True, finalize=True)
